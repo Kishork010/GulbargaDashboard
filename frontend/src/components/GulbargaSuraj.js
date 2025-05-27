659a971f0f9cded28efb96e56016faefd239df17
Kishor Kumar <140684987+Kishork010@users.noreply.github.com>
2025-05-27 10:04:44 +0530

    Update GulbargaSuraj.js

--- frontend/src/components/GulbargaSuraj.js
+++ frontend/src/components/GulbargaSuraj.js
@@ -21,7 +21,7 @@ function SurajSarda() {
 
   const fetchData = async () => {
     try {
-      const res = await fetch('http://localhost:5000/api/green_form');
+      const res = await fetch('https://gulbargadashboard.onrender.com');
       const json = await res.json();
 
       const tlData = json.filter(row => row['tl_name']?.trim() === 'PSS1_300B640');
